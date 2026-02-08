5d619a040d8790af61893909c70034b1f6243b2c
Aman Gaade <ak9070777@gmail.com>
2026-02-09 01:02:02 +0530

    product

---
+++ Product.java
@@ -0,0 +1,9 @@
+ public class Product {
+    public static void main(String[]args){
+        int a = 2;
+        int b = 3;
+        int c = a*b;
+        System.out.println("product of a and b "+ c);
+    }
+}
+ 
