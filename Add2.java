2b97e793a1dc0e92509694eb3e592d0850f0d08f
Aman Gaade <ak9070777@gmail.com>
2026-02-09 00:56:13 +0530

    initial commit

---
+++ Add2.java
@@ -0,0 +1,8 @@
+public class Add2 {
+    public static void main(String[]args){
+        int a = 2;
+        int b = 3;
+        int c = a+b;
+        System.out.println("sum of a and b "+ c);
+    }
+}
