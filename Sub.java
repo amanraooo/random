3a9a50cc1eebcc42080161a3d5eefc21b490389d
Aman Gaade <ak9070777@gmail.com>
2026-02-09 00:59:56 +0530

    subtraction

---
+++ Sub.java
@@ -0,0 +1,9 @@
+ public class Sub {
+    public static void main(String[]args){
+        int a = 2;
+        int b = 3;
+        int c = a-b;
+        System.out.println("diff of a and b "+ c);
+    }
+}
+ 
